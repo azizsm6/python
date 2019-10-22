d47c53f7489b76efd53e3ec6f999555b2a0c9341
azizsm6 <56879168+azizsm6@users.noreply.github.com>
2019-10-22 06:51:14 -0700

    Create test-sysinfo.py
    Under development

---
+++ test-sysinfo.py
@@ -0,0 +1,11 @@
+#System info >>
+# By AZiZ
+import os
+
+env = os.environ.get("OS")
+cdir = os.getcwd()
+pid = os.getpid()
+print "info sys:",env
+print "pwd : ",cdir
+print "pid :",pid
+print "bye :)"
